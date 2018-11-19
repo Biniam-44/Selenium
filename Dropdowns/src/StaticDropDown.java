5e56b722377404ac3c8c0d7fbcbda97ff1bd6215
Nebyou Getaneh <getanehnebyou@gmail.com>
2018-11-19 14:16:58 -0600

    Commit static dropdown test case

--- Dropdowns/src/StaticDropDown.java
+++ Dropdowns/src/StaticDropDown.java
@@ -12,12 +12,19 @@ public class StaticDropDown {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nebiy\\Desktop\\Selenium\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.spicejet.com/");
+		driver.findElement(By.xpath("//*[@id=\'ctl00_mainContent_ddl_originStation1_CTXT\']")).click();
+		driver.findElement(By.xpath("//*[@id=\'dropdownGroup1\']/div/ul[1]/li[6]/a")).click();
+		driver.findElement(By.xpath("(//*[@id='dropdownGroup1']/div/ul[1]/li[11]/a)[2]")).click();
+		//*[@id='comp-jo1ls0l8input'] //*[@id="comp-jo1ls0l8input"]
+		//Select s = new Select (driver.findElement(By.cssSelector("#comp-jo1ljqoxcollection")));
 		
+		//s.selectByValue("Quality Assurance (QA)");
 		
-		driver.get("https://spicejet.com/");
+		/*driver.get("https://spicejet.com/");
 	
 		driver.findElement(By.xpath("//*[@id='ctl00_mainContent_ddl_originStation1']")).click();
-		
+		driver.findElement(By.xpath("//*[@id=\'ctl00_mainContent_ddl_originStation1\']/option[8]")).click(); */
 		
 		/*Select s = new Select (driver.findElement(By.xpath("//*[@id='ctl00_mainContent_DropDownListCurrency']")));
 		
