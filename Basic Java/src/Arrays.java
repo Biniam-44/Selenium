125c6b6fcd8290f747d915f5f1aabd1633ddae82
Nebyou Getaneh <getanehnebyou@gmail.com>
2018-12-04 12:30:41 -0600

    Arrays Commit

--- Basic Java/src/Arrays.java
+++ Basic Java/src/Arrays.java
@@ -4,6 +4,12 @@ public class Arrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// if I want to store multiple values on one container we use Arrays
+		// Arrays are multiple values of the same data type
+		//Key word new allocates memory for an array
+		int a[] = new int[5]; // Declares an array and allocates memory for the values
+		int b[] = new int[10];
+		
 	}
 
 }
